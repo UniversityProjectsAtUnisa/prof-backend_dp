71edabebf6e8ba29c052fa4a42450a2551f5dd37
GiandoAL <giandomenico1998@gmail.com>
2022-02-23 13:41:40 +0100

    fix delle ricerche sia con link che con parola

--- providers/sapere/__init__.py
+++ providers/sapere/__init__.py
@@ -4,10 +4,16 @@ import re
 
 from definitions.scraper import ScraperBase, ScrapeReply, DisamiguousLink
 import logging
+from urllib.parse import urljoin
+
+from typing import Callable, Tuple
+from grpclib.exceptions import GRPCError
+from grpclib.const import Status
 
 class ScraperSapereIT(ScraperBase):
     _BASE_URL = "https://www.sapere.it"
-    _SEARCH_URL = _BASE_URL + "/sapere/search.html?q1="
+    _SEARCH_QUERY = "/sapere/search.html?q1="
+    _SEARCH_URL = _BASE_URL + _SEARCH_QUERY
     def _is_url(self, text: str) -> bool:
         return "/" in text
     
@@ -23,13 +29,14 @@ class ScraperSapereIT(ScraperBase):
         r = requests.get(url)
 
         if r.status_code != 200:
-            return False
+            raise GRPCError(status=Status.INTERNAL, message="Request to provider failed")
 
         soup = bs(r.content, "lxml")
 
         container = soup.find("ul", class_="search-results-list")
         if not container:
-            return False
+            raise GRPCError(status=Status.NOT_FOUND, message="Result not found")
+
         result = []
         for item in container.find_all("li"):
             title_section = item.find("a")
@@ -47,83 +54,115 @@ class ScraperSapereIT(ScraperBase):
         r = requests.get(url)
 
         if r.status_code != 200:
-            return False
+            raise GRPCError(status=Status.INTERNAL, message="Request to provider failed")
         soup = bs(r.content, "lxml")
 
         paragraphs = soup.findAll('div', {"id": re.compile('^p')})
+        logging.info(len(paragraphs))
 
         if len(paragraphs) == 0:
-            return False
+            raise GRPCError(status=Status.NOT_FOUND, message="Result not found")
         
         result = "" 
         for i, paragraph in enumerate(paragraphs):
-            result += paragraph.h2.get_text() + "\n\n"
-            result += paragraph.p.get_text() + ("\n\n" if i < len(paragraphs) - 1 else "")
+            for p in paragraph.childGenerator():
+                if p.name in ["p", "h2"]:
+                    result+=p.getText()  + ("\n\n" if p.name == "h1" else "\n")
+        result = result[:-2]
+        
+        if result == "":
+            raise GRPCError(status=Status.NOT_FOUND, message="Result not found")
 
         return result
 
     def _short_scraping(self, url: str) -> str:
         r = requests.get(url)
+
+        
+
         if r.status_code != 200:
-            return False
+            raise GRPCError(status=Status.INTERNAL, message="Request to provider failed")
+
         soup = bs(r.content, "lxml")
 
         paragraphs = soup.findAll('div', {"id": re.compile('^p')})
 
         if len(paragraphs) == 0:
-            return False
+            raise GRPCError(status=Status.NOT_FOUND, message="Result not found")
         
         result = "" 
+        logging.info(paragraphs[0].p.get_text())
         result += self._cut_to_grammar_point(paragraphs[0].p.get_text()[:1024])
+        logging.info(result)
+        if result == "":
+            raise GRPCError(status=Status.NOT_FOUND, message="Result not found")
         return result
 
-    async def search(self, text: str) -> ScrapeReply:
-        logging.info("")
-
-        r = None
+    def _choose_strategy(self, text:str, is_long:bool) -> Tuple[bool, Callable, str]:
         disambigous = True
+        strategy = None
+        param = ""
 
-        if self._is_url(text):
-            if "seach.html" in text:
-                r = self._short_scraping(self._BASE_URL+text)
+        if self._BASE_URL in text:
+            if self._SEARCH_QUERY in text: #https://www.sapere.it/sapere/search.html?q1=alabama
+                strategy = self._search_scraping
+                param = text
+                disambigous = True
+            else: #https://www.sapere.it/enciclopedia/Alabama+%28Stato%29.html
+                strategy = self._short_scraping if not is_long else self._long_scraping
+                param=text
                 disambigous = False
-            else:
-                r = self._search_scraping(self._BASE_URL+text)
         else:
-            r = self._search_scraping(self._SEARCH_URL+text)
+            if self._SEARCH_QUERY in text: #/search.html?q1=alabama
+                strategy = self._search_scraping
+                param=f"{urljoin(self._BASE_URL, text)}"
+                disambigous = True
+            else: 
+                if self._is_url(text): #/enciclopedia/Alabama+%28Stato%29.html
+                    strategy = self._short_scraping
+                    param=f"{urljoin(self._BASE_URL, text)}"
+                    disambigous = False
+                else: #alabama
+                    strategy = self._search_scraping
+                    param=self._SEARCH_URL + text
+                    disambigous = True
+        logging.info(param)
+
+        return (disambigous, strategy, param)
+
+    async def search(self, text: str) -> ScrapeReply:
+        r = None
+        disambigous, strategy, param = self._choose_strategy(text, False)
+
+        r=strategy(param)
+
+        logging.info(f'r={r}')
 
         if not r:
-            return #TODO: communicate error
+            raise GRPCError(status=Status.NOT_FOUND, message="Result not found")
         
         if disambigous:
             return ScrapeReply(
                 language="it",
                 disambiguous=True,
                 data='',
-                disambiguous_data=[DisamiguousLink(label=item.label, url=item.url) for item in r])
+                disambiguous_data=[DisamiguousLink(label=f"{item['label']}", url=f"{urljoin(self._BASE_URL, item['url'])}") for item in r])
         return ScrapeReply(language="it", disambiguous=False, data=r)
 
 
     async def long_search(self, text: str) -> ScrapeReply:
         r = None
-        disambigous = True
+        disambigous, strategy, param = self._choose_strategy(text, True)
 
-        if self._is_url(text):
-            if "seach.html" in text:
-                r = self._long_scraping(self._BASE_URL+text)
-                disambigous = False
-            else:
-                r = self._search_scraping(self._BASE_URL+text)
-        else:
-            r = self._search_scraping(self._SEARCH_URL+text)
+        r=strategy(param)
 
         if not r:
-            return #TODO: communicate error
+            raise GRPCError(status=Status.NOT_FOUND, message="Result not found")
         
         if disambigous:
             return ScrapeReply(
                 language="it",
                 disambiguous=True,
                 data='',
-                disambiguous_data=[DisamiguousLink(label=item.label, url=item.url) for item in r])
+                disambiguous_data=[DisamiguousLink(label=f"{item['label']}", url=f"{urljoin(self._BASE_URL, item['url'])}") for item in r])
         return ScrapeReply(language="it", disambiguous=False, data=r)
