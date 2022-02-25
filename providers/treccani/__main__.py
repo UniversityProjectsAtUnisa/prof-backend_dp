46a9608ceae4a1c7b61f75a010f19326b2cfb411
Marco Della Rocca <marco741147@gmail.com>
2022-02-25 17:32:20 +0100

    Changed logging format

--- providers/treccani/__main__.py
+++ providers/treccani/__main__.py
@@ -6,5 +6,5 @@ from .. import providers_port_mapping
 from config import LOGGING_LEVEL
 
 if __name__ == '__main__':
-    logging.basicConfig(format=LOGGING_FORMAT, level=LOGGING_LEVEL)
+    logging.basicConfig(format=f"TRECCANI:::{LOGGING_FORMAT}", level=LOGGING_LEVEL)
     ScraperServer(ScraperTreccani(), port=providers_port_mapping["treccani"]).run()
